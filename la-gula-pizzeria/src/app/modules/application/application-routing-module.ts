fdd92e804cba611089e6092c9d43d9626e270257
Frank <fcarpio113@gmail.com>
2025-10-16 21:22:09 -0500

    Agregando configuracion de application module

--- la-gula-pizzeria/src/app/modules/application/application-routing-module.ts
+++ la-gula-pizzeria/src/app/modules/application/application-routing-module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPage } from './pages/layout-page/layout-page';
+import { HomePage } from './pages/home-page/home-page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPage,
+    children: [
+      {
+        path: 'home',
+        component: HomePage
+      },
+      {
+        path: '**',
+        redirectTo: 'home'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
