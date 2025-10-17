36ea057d9f27d23475de0eff2ebec7216b59207d
Frank <fcarpio113@gmail.com>
2025-10-16 22:30:53 -0500

    Agreganddo header y footer to application

--- la-gula-pizzeria/src/app/modules/application/pages/layout-page/layout-page.ts
+++ la-gula-pizzeria/src/app/modules/application/pages/layout-page/layout-page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "../../components/header/header";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-layout-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './layout-page.html',
   styleUrl: './layout-page.css'
 })
