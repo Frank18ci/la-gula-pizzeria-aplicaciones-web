77a9ca3dda287a9ef911e1841872925f3de92d8b
Frank <fcarpio113@gmail.com>
2025-10-18 08:51:03 -0500

    Agregando excepciones de autorización para métodos GET y rutas públicas

--- back end/gateway-service/src/main/java/com/cibertec/gatewayservice/security/util/JwtAuthFilter.java
+++ back end/gateway-service/src/main/java/com/cibertec/gatewayservice/security/util/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -22,6 +23,12 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
+
+        if (HttpMethod.GET.matches(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
